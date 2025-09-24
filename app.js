96edefdbcdf4ec37f67d733023e75962d69f5c67
Litzy Espinoza <espinozalitzy4@hotmail.com>
2025-09-23 20:15:43 -0600

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -15,3 +15,14 @@ function agregarAmigo() {
   input.value = "";
   mostrarAmigos(); // muestra la lista actualizada en el HTML
 }
+
+function mostrarAmigos() {
+  let lista = document.getElementById("listaAmigos"); 
+  lista.innerHTML = "";                               
+
+  for (let i = 0; i < amigos.length; i++) {          
+    let li = document.createElement("li");           
+    li.textContent = amigos[i];                      
+    lista.appendChild(li);                           
+  }
+}
